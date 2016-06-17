c91d7e6da3d209d4370cc011e1831da7dc4e9db9
D24809 <darshan.naik@eon.com>
2016-06-17 15:24:14 +0100

    Added support for onload method like ng-include

--- partial.js
+++ partial.js
@@ -1,41 +1,46 @@
-  var partial = (function () {
-            var partials = document.getElementsByTagName("partial");
-            console.log(partials.length);
+var partial = (function () {
+    var partials = document.getElementsByTagName("partial");
+    console.log(partials.length);
 
 
-            var getPage = function (url, elem, callback) {
-                var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-                xhr.open('GET', url);
-                xhr.onreadystatechange = function () {
-                    if (xhr.readyState > 3 && xhr.status == 200) {
-                        elem.innerHTML = xhr.responseText;
-                        var scripts = elem.getElementsByTagName('script');
-                        for (var ix = 0; ix < scripts.length; ix++) {
-                            eval(scripts[ix].text);
-                        }
-                        if (callback !== undefined) {
-                            /*handles foundation initialization*/
-                            $(document).foundation();
-                            callback(elem);
-                        }
-                    }
-                    ;
-                };
-                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-                xhr.send();
-                return xhr;
+    var getPage = function (url, elem, callback) {
+        var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState > 3 && xhr.status == 200) {
+                elem.innerHTML = xhr.responseText;
+                var scripts = elem.getElementsByTagName('script');
+                for (var ix = 0; ix < scripts.length; ix++) {
+                    eval(scripts[ix].text);
+                }
+                if (callback !== undefined) {
+                    /*handles foundation initialization*/
+                    callback(elem);
+                }
             }
+            ;
+        };
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        xhr.send();
+        return xhr;
+    }
 
-            for (var index = 0; index < partials.length; ++index) {
-                var item = partials[index];
+    for (var index = 0; index < partials.length; ++index) {
+        var item = partials[index];
 
-                var src = item.getAttribute('src');
-                getPage(src, item);
-            }
+        var src = item.getAttribute('src');
+        var func = item.getAttribute('onload');
+        func = eval(func);
+
+        if (func == null || func == undefined)
+            getPage(src, item);
+        else
+            getPage(src, item, func)
+    }
 
 
-            return {
-                getPage: getPage
-            }
-        })();
+    return {
+        getPage: getPage
+    }
+})();
 
