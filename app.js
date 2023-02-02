129da7e67ea1cb71bbc214e7a6c8b73166f4cd5d
darshanmarathe <darshanmarathe@gmail.com>
2023-02-03 05:18:32 +0530

    added stuff and setup npr

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+const port = process.env.PORT ||  3000;
+
 app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
-app.listen(80);
+// app.use(express.static(path.join(__dirname, 'css'))); //  "public" off of current is root
+
+// app.use(express.static(path.join(__dirname, 'images'))); //  "public" off of current is root
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+app.listen(port);
